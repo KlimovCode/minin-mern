d61489a8cca928590796f8beaee9c5216c8ee0a4
klimovcode <klimovcode@mail.ru>
2020-11-20 08:11:43 +0300

    add config package and setting

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
-
 const app = express()
+const config = require('config')
+
+const PORT = config.get('port') || 5001
 
 app.listen(5000, () => {
-  console.log('Server has started')
+  console.log('Server has started on port '+PORT)
 })
