25cdbf0af06b4ceea004e2b9d5f3256d286ab2a7
klimovcode <klimovcode@mail.ru>
2020-11-21 19:29:01 +0300

    add mongodb and setting

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const config = require('config')
+const mongoose = require('mongoose')
 
 const PORT = config.get('port') || 5001
 
-app.listen(5000, () => {
-  console.log('Server has started on port '+PORT)
-})
+async function start() {
+  await mongoose.connect(config.get('dbURI'), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  app.listen(5000, () => {
+    console.log('Server has started on port '+PORT)
+  })
+}
+  
+start()
