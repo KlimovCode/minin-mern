80f015d2881a8c0dd988ff39a39291bbd3f2be26
klimovcode <klimovcode@mail.ru>
2020-11-24 12:02:19 +0300

    register router done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,19 @@
 const {Router} = require('express')
 const router = Router()
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
 
 router.post('register', async (req, res) => {
   try {
     const { email, password } = req.body
+    const candidate = await User.findOne({email})
+    if(candidate) 
+      return res.status(400).json({msg:'user exist'})
+    const hashedPassword = await bcrypt.hash(password, 12)
+    const user = 
+      await new User({email, password: hashedPassword})
+    await user.save()
+    res.status(201).json({msg:'user saved'})
   } catch(e) {
     res.status(500).json({ msg: 'something went wrong' })
   }
