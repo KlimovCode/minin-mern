628da2a0676b03e81b3ad1a5ca61831f0c764ea6
klimovcode <klimovcode@mail.ru>
2020-11-24 12:26:58 +0300

    register router - add validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,23 @@
 const {Router} = require('express')
 const router = Router()
 const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+
 const User = require('../models/User')
 
-router.post('register', async (req, res) => {
+router.post('register', 
+  [
+    check('email', 'not correct email').isEmail(),
+    check('password', '6 symbols min').isLength({min:6})
+  ],
+  async (req, res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+      msg: 'not correct data'
+    })
+  }
   try {
     const { email, password } = req.body
     const candidate = await User.findOne({email})
