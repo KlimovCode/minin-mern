6ae4552656d0b81e292bff0978743f8895b9f637
klimovcode <klimovcode@mail.ru>
2020-11-25 08:59:20 +0300

    AuthPage basic layout

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -4,6 +4,20 @@ export const AuthPage = () => {
   return (
   <div>
     <h1>Auth</h1>
+    <div>
+      <label htmlFor="email"> Email </label> <br />
+      <input id="email" />
+    </div>
+    <div>
+      <label htmlFor="password"> Password </label> <br />
+      <input id="password" />
+    </div>
+ <br /> <br />
+    <div>
+      <button> Log in </button>
+    &nbsp;
+      <button> Sign up </button>
+    </div>
   </div>
   )
 }
