1f60d011861ecdc94eee2859e0ce0ca8b5361569
klimovcode <klimovcode@mail.ru>
2020-11-26 07:39:38 +0300

    AuthPage valueHandler done FLUX

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AuthPage = () => {
+  const [value, setValue] = useState({
+    email: '', password:  ''
+  })
+
+  const valueHandler = (event) => {
+    setValue({
+      ...value,
+      [event.target.name]: event.target.value
+    })
+  }
+
   return (
   <div>
     <h1>Auth</h1>
     <div>
       <label htmlFor="email"> Email </label> <br />
-      <input id="email" />
+      <input id="email" name="email" 
+        onChange={valueHandler}
+      />
     </div>
     <div>
       <label htmlFor="password"> Password </label> <br />
-      <input id="password" />
+      <input id="password" name="password"
+        onChange={valueHandler}
+      />
     </div>
  <br /> <br />
     <div>
