ae038921e59ff200975b17c104c13acb6f2e631f
klimovcode <klimovcode@mail.ru>
2020-11-29 21:13:53 +0500

    create AuthContext for delivery data through App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { useRoutes } from './routes'
+import { useAuth } from './hooks/auth.hook'
+
 import 'materialize-css'; // It installs the JS asset only
-import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/css/materialize.min.css'
 
 function App() {
+  const { login, logout, token, userId } = useAuth()
   const router = useRoutes()
   return (
     <BrowserRouter>
