8de4d0213e65c505de5ef977da20f8e19abe0f21
klimovcode <klimovcode@mail.ru>
2020-11-30 00:24:24 +0500

    layout CreatePage

--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const CreatePage = () => {
+  const [value, setValue] = useState('')
+
+
   return (
     <div>
       <h1>CreatePage</h1>
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} /> <br />
+      <button>Create short link</button>
     </div>
   )
 }
