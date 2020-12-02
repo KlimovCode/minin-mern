46a5c82f5fcdfd0032ea19c003e19614b56a320f
klimovcode <klimovcode@mail.ru>
2020-12-02 21:25:34 +0300

    LinksPage update

--- client/src/components/LinksList.jsx
+++ client/src/components/LinksList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export const LinksList = ({links})=>{
   if(!links.length) return <p>have not links yet</p>
@@ -11,6 +12,11 @@ export const LinksList = ({links})=>{
         <td>{el.from}</td>
         <td>{el.to}</td>
         <td>{el.clicks}</td>
+        <td>
+          <NavLink to={'/api/links/'+el._id}>
+            {el._id}
+          </NavLink>
+        </td>
       </tr>
       )
     })
