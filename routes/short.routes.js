504a7696515d7598c3f1eb0b2f55671bf90bbf37
klimovcode <klimovcode@mail.ru>
2020-12-04 20:30:33 +0500

    fix clicks

--- routes/short.routes.js
+++ routes/short.routes.js
@@ -6,6 +6,7 @@ const Link = require('../models/Link')
 router.get('/:id', async (req, res) => {
   try {
     const link = await Link.findOne({ code: req.params.id })
+    await Link.findByIdAndUpdate(link._id, { clicks: link.clicks + 1 })
     res.redirect(link.from)
   } catch (error) {
     res.status(500).json({ msg: 'something went wrong' })
