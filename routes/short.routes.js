4cacf062839d5c7b765d85cfd371bcf724a67f8b
klimovcode <klimovcode@mail.ru>
2020-12-04 20:37:29 +0500

    update clicks function

--- routes/short.routes.js
+++ routes/short.routes.js
@@ -3,10 +3,12 @@ const router = Router()
 
 const Link = require('../models/Link')
 
-router.get('/:id', async (req, res) => {
+router.get('/:code', async (req, res) => {
   try {
-    const link = await Link.findOne({ code: req.params.id })
-    await Link.findByIdAndUpdate(link._id, { clicks: link.clicks + 1 })
+    const link = await Link.findOne({ code: req.params.code })
+    // await Link.findByIdAndUpdate(link._id, { clicks: link.clicks + 1 })
+    link.clicks++
+    link.save()
     res.redirect(link.from)
   } catch (error) {
     res.status(500).json({ msg: 'something went wrong' })
