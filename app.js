75cab5cd0b3483ecf9cc4af0b81428e96babc520
klimovcode <klimovcode@mail.ru>
2020-12-05 13:04:55 +0500

    add production settings

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/short.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(__dirname, 'client', 'build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = config.get('port') || 5002
 
